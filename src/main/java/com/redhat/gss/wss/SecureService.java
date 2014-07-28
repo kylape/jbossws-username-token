baffbfb472c89f030f03b90a3958a1753dec2612
Kyle Lape <kyle.lape@redhat.com>
2014-07-28 10:26:53 -0700

    Converting to ejb jar

--- src/main/java/com/redhat/gss/wss/SecureService.java
+++ src/main/java/com/redhat/gss/wss/SecureService.java
@@ -1,17 +1,23 @@
 package com.redhat.gss.wss;
 
+import javax.ejb.Stateless;
+import javax.jws.WebService;
+
 import org.apache.cxf.interceptor.InInterceptors;
 import org.apache.cxf.annotations.Logging;
 import org.jboss.ws.api.annotation.EndpointConfig;
 import org.jboss.logging.Logger;
+import org.jboss.ejb3.annotation.SecurityDomain;
 
-@javax.jws.WebService(wsdlLocation="WEB-INF/wsdl/secureService.wsdl")
-@EndpointConfig(configFile = "WEB-INF/jaxws-endpoint-config.xml", configName = "Custom WS-Security Endpoint")
+@WebService(wsdlLocation="META-INF/wsdl/secureService.wsdl")
+@EndpointConfig(configFile = "META-INF/jaxws-endpoint-config.xml", configName = "Custom WS-Security Endpoint")
 @InInterceptors(interceptors = {
   "org.jboss.wsf.stack.cxf.security.authentication.SubjectCreatingPolicyInterceptor",
   "com.redhat.gss.wss.MyAuthorizationInterceptor"
 })
 @Logging(pretty=true)
+@Stateless
+@SecurityDomain("other")
 public class SecureService
 {
   private static Logger log = Logger.getLogger(SecureService.class);
